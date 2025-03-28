dc821beb477a10cfcdea92a0cacc5fc931238852
andre <andrecastor828@gmail.com>
2025-03-28 11:49:12 -0300

    tipos de variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -21,4 +21,17 @@ console.log(exemplo1)
 
 //TIPOS DE VARIAVEIS
 
-let
+let ex1 = "Fiap";
+console.log(typeof ex1)
+
+let ex2 = 25
+console.log(typeof ex2)
+
+let ex3 =  true
+console.log(typeof ex3)
+
+let ex4={};
+console.log(typeof ex4)
+
+let ex5=[];
+console.log(typeof ex5)
