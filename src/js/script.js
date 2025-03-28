41a947583d3d966e8ee2863fcdfde542b104f997
andre <andrecastor828@gmail.com>
2025-03-28 11:41:45 -0300

    declaração de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+//DECLARAÇÃO E VARIAVEOS 
+
+var nome = "Fiap";
+console.log(nome)   
+
+let idade = "19";
+console.log(idade)
+
+const sobrenome ="spinola" 
+console.log(sobrenome)
+
+//undefined 
+
+let exemplo;
+console.log(exemplo)
+
+//nuk=la
+
+let exemplo1 =null
+console.log(exemplo1)
+
+//TIPOS DE VARIAVEIS
+
+let
